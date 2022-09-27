5f1193c91392b5e0de3bf9cf1b0cbeac45250c2d
MThanaj <mariathanaj@gmail.com>
2022-09-27 15:42:52 +0100

    update

--- R/permFL_fast.R
+++ R/permFL_fast.R
@@ -21,7 +21,7 @@
 #' @export
 #' @examples
 #' \dontrun{
-#' TFCEresults = permFL_fast(X, Y, extract, A, NNmatrix, nPermutations = 1000, E=0.5, H=2)
+#' TFCEresults = permFL_fast(X, Y, extract, A, as.matrix(NNmatrix), nPermutations = 1000, E=0.5, H=2)
 #' }
 
 permFL_fast <- function(X, Y, extract, A, NNmatrix, nPermutations, E = 0.5, H = 2){
